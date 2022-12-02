499b824af43228d23efbf2c62c11014b66b3df0b
Tim Moore <tim.moore@genomicsplc.com>
2022-12-02 22:15:17 +0000

    Refactor for part 2.

--- main/2022-2/2022-2.go
+++ main/2022-2/2022-2.go
@@ -15,7 +15,7 @@ func openInput(path string) *os.File {
 	return f
 }
 
-func getScore(text string) (int, error) {
+func getScore1(text string) (int, error) {
 	switch text {
 	case "A X":
 		return 4, nil
@@ -40,20 +40,52 @@ func getScore(text string) (int, error) {
 	}
 }
 
+func getScore2(text string) (int, error) {
+	switch text {
+	case "A X":
+		return 3, nil
+	case "A Y":
+		return 4, nil
+	case "A Z":
+		return 8, nil
+	case "B X":
+		return 1, nil
+	case "B Y":
+		return 5, nil
+	case "B Z":
+		return 9, nil
+	case "C X":
+		return 2, nil
+	case "C Y":
+		return 6, nil
+	case "C Z":
+		return 7, nil
+	default:
+		return -1, errors.New(fmt.Sprintf("Cannot parse %v", text))
+	}
+}
+
 func main() {
 	f := openInput("main/2022-2/input1.txt")
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
-	totalScore := 0
+	totalScore1, totalScore2 := 0, 0
 	for scanner.Scan() {
 		text := scanner.Text()
-		score, getScoreErr := getScore(text)
+		score, getScoreErr := getScore1(text)
 		if getScoreErr != nil {
 			panic(getScoreErr)
 		}
-		totalScore += score
+		totalScore1 += score
+
+		score, getScoreErr = getScore2(text)
+		if getScoreErr != nil {
+			panic(getScoreErr)
+		}
+		totalScore2 += score
 	}
 
-	fmt.Println(totalScore)
+	fmt.Println(totalScore1)
+	fmt.Println(totalScore2)
 }
